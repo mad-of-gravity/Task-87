bc2aa46a454ea17eb2ccf4b42410c5a0a910966f
mad-of-gravity <mad.of.gravity@gmail.com>
2023-01-19 00:11:59 +0200

    add fetched classes to each element

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,25 @@
 import "../scss/app.scss";
+import {pluck} from "ramda";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+  //For plucking array data via ramda function pluck
+  const getClassNameArray = pluck("class");
 
   const arrayToPluck = [
     { name: "John", class: "is-primary" },
     { age: 23, class: "is-warning" },
     { job: "programmer", class: "is-danger" },
   ];
+
+  const classArray = getClassNameArray(arrayToPluck);
+
+  //Adds the fetched classes to each element
   const articles = document.querySelectorAll("article");
+  articles.forEach((article, index) => {
+    article.classList.add(classArray[index]);
+  })
+
+  console.log(classArray);
 });
